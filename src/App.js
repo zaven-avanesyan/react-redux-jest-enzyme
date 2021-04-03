620ef42ae1dcc3ecea3073ea1b8922995f7a5d3a
zaven.avanesyan <zaven.avanesyan@everymatrix.com>
2021-04-03 02:28:09 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./component/header";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/header/spec.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+
+describe("Header Component", () => {
+  it("should render without errors", () => {
+    expect(1).toBe(1);
+  });
+});
